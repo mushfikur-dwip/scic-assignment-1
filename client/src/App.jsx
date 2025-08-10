6d94a9cd61db05016ef6e860ff44b84c964dcd7b
mushfikur-dwip <musfikurrahmandip@gmail.com>
2025-08-10 14:18:43 +0600

    fix: remove unnecessary newline in App component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ const App = () => {
       <div>
         <h1 className="text-3xl bg-amber-300 font-bold underline"> Welcome to Vite + React + TailwindCSS!</h1>
         <p className="mt-4 text-lg">This is a simple setup to get started with Vite, React, and TailwindCSS.</p>
-
+ 
       </div>
     </>
   );
